3bcb593ec5c38f87af960432028ebefc39fe5054
techguy200 <67910741+techguy200@users.noreply.github.com>
2023-09-12 10:51:18 -0400

    motor wrapper classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorcontrol/dualmotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorcontrol/dualmotor.java
@@ -3,21 +3,26 @@ package org.firstinspires.ftc.teamcode.motorcontrol;
 import com.ThermalEquilibrium.homeostasis.Controllers.Feedback.PIDEx;
 import com.ThermalEquilibrium.homeostasis.Parameters.PIDCoefficientsEx;
 import com.arcrobotics.ftclib.controller.PIDFController;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 public class dualmotor {
-    DcMotorEx m;
-    DcMotorEx m1;
+    DcMotor m;
+    DcMotor m1;
     PIDCoefficientsEx pid;
     PIDEx pidc;
     PIDFCoefficients pidf;
     PIDFController pidfc;
-    public void dualmotor(String m,String m1,HardwareMap hardwareMap){
-        this.m=hardwareMap.get(DcMotorEx.class, m);
-        this.m1=hardwareMap.get(DcMotorEx.class, m1);
 
+    public void dualmotorinit(String m,String m1,HardwareMap hardwareMap){
+        this.m=hardwareMap.dcMotor.get(m);
+        this.m1=hardwareMap.dcMotor.get(m1);
+        this.m.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.m1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.m1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        this.m.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
     public void setPower(double p){
         m.setPower(p);
@@ -30,17 +35,18 @@ public class dualmotor {
 
     }
 
-    public void runPID(double target,double current){
-        m.setPower(pidc.calculate(target, current));
-        m1.setPower(pidc.calculate(target, current));
+    public void runPID(double target){
+        m.setPower(pidc.calculate(target, m.getCurrentPosition()));
+        m1.setPower(m.getPower());
     }
     public void setPIDF(double p, double i, double d, double f, double t){
         pidfc= new PIDFController(p,i,d,f);
         pidfc.setTolerance(t);
     }
-    public void runPIDF(double target,double current){
-        m.setPower(pidfc.calculate(target, current));
-        m1.setPower(pidfc.calculate(target, current));
+    public void runPIDF(double target){
+        pidfc.setSetPoint(target);
+        m.setPower(pidfc.calculate(m.getCurrentPosition()));
+        m1.setPower(m.getPower());
     }
 
 
